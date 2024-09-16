62e7120085f33dc6a001b25f71a218e9a1fd0f8b
reiltusoy331 <reiltusoy331@gmail.com>
2024-09-17 06:20:49 +0800

    Adding 11-If-else.js to my JavaScripts github repo.

---
+++ 11-If-else.js
@@ -0,0 +1,13 @@
+let hour = 10;
+
+if(hour >= 6 && hour <12){
+    console.log('Good morning!');
+}
+
+else if(hour >= 12 && hour <6){
+    console.log('Good evening!');
+}
+else
+    console.log('Good evening!');
+
+
