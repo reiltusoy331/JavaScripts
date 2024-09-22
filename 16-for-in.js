cb2e078bbbcc2d0fdc3293a5550389d18f113e99
reiltusoy331 <reiltusoy331@gmail.com>
2024-09-23 01:43:54 +0800

    Adding 16-for-in.js to my JavaScripts github repo.

---
+++ 16-for-in.js
@@ -0,0 +1,14 @@
+
+const person ={
+    name:'Mosh',
+    age:30
+};
+
+for (let key in person)
+    console.log(key, person[key])
+
+
+const colors=['blue','red','green']
+
+for (let index in colors)
+    console.log(index,colors[index])
