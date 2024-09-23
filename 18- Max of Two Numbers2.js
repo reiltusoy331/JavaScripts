690b7c5053da84a95b1a58394c60bf8fb272c4d2
reiltusoy331 <reiltusoy331@gmail.com>
2024-09-24 06:13:34 +0800

    Adding 18-MaxofTwoNumbers2.js to my JavaScripts github repo.

---
+++ 18- Max of Two Numbers2.js
@@ -0,0 +1,15 @@
+
+///////////
+// Option 2
+///////////
+
+let number = max_two_num(2,3);
+console.log(number);
+
+
+function max_two_num(num1,num2){
+    return (num1>num2) ? num1:num2    //ternary operator
+    
+}
+
+
