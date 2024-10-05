b09f7d6fbc2bbe3d3bac24d9b54b864208b50b99
reiltusoy331 <reiltusoy331@gmail.com>
2024-10-05 19:52:34 +0800

    Adding 22- Exercise- Even and Odd Numbers1.js to my JavaScripts github repo.

---
+++ 22- Exercise- Even and Odd Numbers1.js
@@ -0,0 +1,14 @@
+
+showNumbers(10);
+
+
+function showNumbers(limit){
+    for (let i=0;i<=limit;i++){
+        if(i %2==!0)
+            console.log(i,'ODD')
+        else
+            console.log(i,'EVEN')
+    }
+     
+
+}
