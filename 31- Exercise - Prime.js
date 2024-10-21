586b8ea216c2055dea815a47cd54e548a7ca5196
reiltusoy331 <reiltusoy331@gmail.com>
2024-10-21 12:31:53 +0800

    Adding 31-Exercise-Prime.js to my JavaScripts github repo.

---
+++ 31- Exercise - Prime.js
@@ -0,0 +1,12 @@
+
+showPrimes(40);
+
+function showPrimes(limit){
+
+    for(let i=1;i<limit;i++){
+        if(i > 1){
+            if(i %2!=0 && i %3!=0)
+                console.log(i)
+    }
+    }
+}
