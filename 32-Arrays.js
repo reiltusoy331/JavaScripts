86720b510c95d7506d3211577a232e95fef9c61b
reiltusoy331 <reiltusoy331@gmail.com>
2024-10-26 23:45:15 +0800

    Adding 32-Arrays.js to my JavaScripts github repo.

---
+++ 32-Arrays.js
@@ -0,0 +1,6 @@
+//Creating an Array
+let names = ["David","Kim","Steve","Katie"];
+
+// Accessing values of an array
+console.log(`Names Length: ${names.length}`);
+console.log(`First Name: ${names[0]}`);
