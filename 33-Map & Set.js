bf2308bb8cb6e0898ee6e619cbb9e1b24db2ab19
reiltusoy331 <reiltusoy331@gmail.com>
2024-10-26 23:45:57 +0800

    Adding 33-Map

---
+++ 33-Map & Set.js
@@ -0,0 +1,12 @@
+//Create object
+
+const obj = {
+    firstName:"David",
+    lastName:"Tucker"
+};
+
+// Creating a Map
+const map1 = new Map()
+map1.set("firstName","David");
+map1.set("lastName","Tucker");
+console.log(map1)
