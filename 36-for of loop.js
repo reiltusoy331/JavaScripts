0bc50893535faad413baf0369fe66ecf75c670e1
reiltusoy331 <reiltusoy331@gmail.com>
2024-10-28 11:49:34 +0800

    Adding 36-for-of-loop.js to my JavaScripts github repo.

---
+++ 36-for of loop.js
@@ -0,0 +1,9 @@
+// Using a for..of loop
+
+// Array of Data
+const names =["David","Kim","Steve","Katie"];
+
+for (let name of names ){
+    // console.log(`${name}`)
+    process.stdout.write(`${name}, `);
+}
