cd10cdea0ad83ec5309b7de753a58c34b4cab5ba
reiltusoy331 <reiltusoy331@gmail.com>
2024-12-01 17:01:59 +0800

    Adding 42-ArrayIndexOfInclude.js to my JavaScripts github repo.

---
+++ 42-ArrayIndexOfInclude.js
@@ -0,0 +1,17 @@
+// Useful Methods in Arrays
+
+let fruits = ['apple','banana','orange','watermelon']
+
+
+//indexOf
+let findApple = fruits.indexOf('orange')
+console.log(findApple)
+
+
+//Index
+let FindInclude = fruits.includes('watermelon')
+console.log(FindInclude)
+
+//slice
+let SliceFruits = fruits.slice(1,3)
+console.log(SliceFruits)
